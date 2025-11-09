8fcca6862dfd3148e319de37bb1c9fe6744d7eaa
Youssef <youssef.afify122@gmail.com>
2025-11-09 17:00:29 +0200

    add more methods to the concerns

--- src/Concerns/Resources/Pages/CreateRecordWithService.php
+++ src/Concerns/Resources/Pages/CreateRecordWithService.php
@@ -9,6 +9,40 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRecordWithService extends CreateRecord
 {
 
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (method_exists(static::getResource(), 'mutateFormDataBeforeCreate')) {
+            $data = static::getResource()::mutateFormDataBeforeCreate($data);
+        }
+        return parent::mutateFormDataBeforeCreate($data);
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (method_exists($this, 'beforeSaveRecord')) {
+            return $this->mutateFormDataBeforeSave($data);
+        }
+
+        if (method_exists(static::getResource(), 'mutateFormDataBeforeSave')) {
+            return static::getResource()::mutateFormDataBeforeSave($data);
+        }
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        if (method_exists($this, 'beforeFillForm')) {
+            return $this->beforeFillForm($data);
+        }
+
+        if (method_exists(static::getResource(), 'mutateFormDataBeforeFill')) {
+            return static::getResource()::mutateFormDataBeforeFill($data);
+        }
+
+        return $data;
+    }
+
     protected function handleRecordCreation(array $data): Model
     {
         $record = self::getResource()::service()->create($data);
